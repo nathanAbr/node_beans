628b3312a2b253263709bb00a1b5986e90686a49
Nathan ABRAHAM <nathan.abraham@outlook.fr>
2018-05-15 15:57:35 +0200

    solve router problems

--- app.js
+++ app.js
@@ -4,8 +4,6 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./app/route');
-
 var app = express();
 
 // view engine setup
@@ -18,7 +16,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+app.use(require('./app/route'));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- app/route.js
+++ app/route.js
@@ -1,2 +1,9 @@
 const express = require('express'),
     router = express.Router();
+
+let mainController = require('./controllers/home_controller');
+
+router.get('/', mainController.home);
+
+module.exports = router;
+
