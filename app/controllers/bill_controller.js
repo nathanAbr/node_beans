b196a670d20ff7953f0ff5c73f0c4c4268f8ed34
LEPOIVRE Jules <jlepoivre@saki-studio.com>
2018-05-16 16:31:00 +0200

    merging add branch

--- app/controllers/bill_controller.js
+++ app/controllers/bill_controller.js
@@ -6,8 +6,6 @@ const errorHandler = require('../error_management');
 let currentDate = new Date();
 let currentYear = currentDate.getFullYear();
 
-
-
 function updateBill(req, res) {
     var val = verifyValues(req);
     if(!val) {
@@ -20,13 +18,15 @@ function updateBill(req, res) {
 }
 
 function listInBill(req, res) {
-
+    
     if(typeof req.get("year") !== 'undefined' && req.get("year") !== "" && req.get("year") !== null){
+        services.recapInBills(req.get("year"));
         services.listInBill(req.get("year")).then((bills)=>{
             console.log(bills);
             res.render('bills_view', {bills:bills});
         });
     } else {
+        services.recapInBills(currentYear);
         services.listInBill(currentYear).then((bills)=>{
             console.log(bills);
             res.render('bills_view', {bills:bills});
