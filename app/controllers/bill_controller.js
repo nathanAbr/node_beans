b5c3fd83b01c5871dc0165388e2d06e3432385e8
Nathan ABRAHAM <nathan.abraham@outlook.fr>
2018-05-16 13:35:00 +0200

    add partials

--- app/controllers/bill_controller.js
+++ app/controllers/bill_controller.js
@@ -3,8 +3,6 @@ const services = require('../services/bill_service');
 let currentDate = new Date();
 let currentYear = currentDate.getFullYear();
 
-function Controller(){}
-
 function listInBill(req, res) {
     if(typeof req.get("year") !== 'undefined' && req.get("year") !== "" && req.get("year") !== null){
         services.listInBill(req.get("year")).then((bills)=>{
