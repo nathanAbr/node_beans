f74a1173ab0d1527d89fedc2503f45d9b8436c31
Nathan ABRAHAM <nathan.abraham@outlook.fr>
2018-05-16 09:24:40 +0200

    update mongo request

--- app/services/bill_service.js
+++ app/services/bill_service.js
@@ -1,6 +1,8 @@
 let Bill =  require('../../models/bill');
 
 function listInBill(year) {
+    console.log(new Date(year, 1, 1));
+    console.log(new Date(year, 12, 31));
     return Bill.find({date_prestation:{"$gte":new Date(year, 1, 1), "$lte":new Date(year, 12, 30)}});
 }
 
