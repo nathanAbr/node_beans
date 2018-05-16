dafdd638a3fb575099e275bd00c09f83af753a0c
Nathan ABRAHAM <nathan.abraham@outlook.fr>
2018-05-16 10:32:11 +0200

    modify service to return promise

--- app/controllers/bill_controller.js
+++ app/controllers/bill_controller.js
@@ -7,12 +7,12 @@ function Controller(){}
 
 function listInBillSI(req, res) {
     if(typeof req.get("year") !== 'undefined' && req.get("year") !== "" && req.get("year") !== null){
-        services.listInBill(req.get("year")).then((bills)=>{
+        services.listInBillSI(req.get("year")).then((bills)=>{
             console.log(bills);
             res.render('bills_view', {bills:bills});
         });
     } else {
-        services.listInBill(currentYear).then((bills)=>{
+        services.listInBillSI(currentYear).then((bills)=>{
             console.log(bills);
             res.render('bills_view', {bills:bills});
         });
--- app/services/bill_service.js
+++ app/services/bill_service.js
@@ -4,7 +4,7 @@ function listInBillSI(year) {
     firstDate = new Date(year, 0, 1);
     lastDate = new Date(year, 11, 31);
     return Bill.find({
-        action_date: {$gt: new Date(firstDate.toISOString()), $lt: new Date(lastDate.toISOString())},
+
         type: "Dev"
     }).populate("customer");
 }
