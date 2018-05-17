b383832238aea979745b72bd465e42f437d442d5
Nathan ABRAHAM <nathan.abraham@outlook.fr>
2018-05-17 09:24:13 +0200

    update

--- app/controllers/bill_controller.js
+++ app/controllers/bill_controller.js
@@ -23,12 +23,10 @@ function listInBill(req, res) {
 
     if(typeof req.get("year") !== 'undefined' && req.get("year") !== "" && req.get("year") !== null){
         services.listInBill(req.get("year")).then((bills)=>{
-            console.log(bills);
             res.render('bills_view', {bills:bills});
         });
     } else {
         services.listInBill(currentYear).then((bills)=>{
-            console.log(bills);
             res.render('bills_view', {bills:bills});
         });
     }
@@ -37,12 +35,10 @@ function listInBill(req, res) {
 function listOutBill(req, res) {
     if(typeof req.get("year") !== 'undefined' && req.get("year") !== "" && req.get("year") !== null){
         services.listOutBill(req.get("year")).then((bills)=>{
-            console.log(bills);
             res.render(res, 'bills_view', {bills:bills});
         });
     } else {
         services.listOutBill(currentYear).then((bills)=>{
-            console.log(bills);
             res.render('bills_view', {bills:bills});
         });
     }
--- app/controllers/customer_controller.js
+++ app/controllers/customer_controller.js
@@ -2,17 +2,16 @@ const services = require('../services/customer_service');
 
 function customersList(req, res) {
         services.customersList().then( (customers)=>{
-            console.log(customers);
             res.render('customers_view', {customers:customers});
         });
 }
 
 function getOne(req, res){
-    console.log(req);
-    // services.getOne(req.get("id")).then((customer)=>{
-    //     console.log(customer);
-    //     res.send(customer);
-    // });
+    services.getOne(req.get("id")).then((customer)=>{
+        res.send(customer);
+    }).catch((err)=>{
+        console.log(err);
+    });
 }
 
 module.exports = {
