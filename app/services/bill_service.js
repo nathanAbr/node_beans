81368f0fb75e6f2bcd40bad299ce01c382543f8d
Nathan ABRAHAM <nathan.abraham@outlook.fr>
2018-05-18 16:49:09 +0200

    update aggregatte

--- app/services/bill_service.js
+++ app/services/bill_service.js
@@ -113,20 +113,57 @@ function recapOutBills(year){
             "$match":{
                 "provider":{$exists:false},
                 "customer":{$exists:true},
-                "action_date": {$gt: firstDate, $lt: lastDate}
+                "action_date": {$gt: firstDate, $lt: lastDate},
             }
         },
-       {
-            "$group":{
-                _id: { year: { $year: "$action_date" } },
-                totalAmount: { $sum: "$amount" },
-                /*totalVAT: {},*/
-                count: { $sum: 1 }
+        {
+            "$group": {
+                _id: {year: {$year: "$action_date"}},
+                totalAmount: {$sum: "$amount"},
+                day_number: {$sum: "$day_number"},
+                count: {$sum: 1},
+                payed: {
+                    $sum: {
+                        $cond: [{
+                            $gt: ["$payment_date", null]
+                        }, "$amount", 0]
+                    }
+                },
+                due: {
+                    $sum: {
+                        $cond: [{
+                            $lt: ["$payment_date", null]
+                        }, "$amount", 0]
+                    }
+                },
+                todo: {
+                    $sum: {
+                        $cond: [{
+                            $lt: ["$billing_date", null]
+                        }, "$amount", 0]
+                    }
+                },
+            },
+        },
+        {
+            "$project": {
+                vat_amount:{
+                    $divide:[{$multiply:[
+                            "$amount",
+                            "$vat"
+                        ]},100]
+                },
+                todo: 1,
+                due: 1,
+                payed: 1,
+                totalAmount: 1,
+                count: 1,
+                day_number: 1,
             }
-        }
+        },
     ]).exec();
-    
 }
+
 module.exports = {
     findOneBill: findOneBill,
     billSelect: billSelect,
@@ -135,6 +172,6 @@ module.exports = {
     listOutBill: listOutBill,
     processUpdateBill : processUpdateBill,
     recapInBills: recapInBills,
-    recapOutBills: recapOutBills
+    recapOutBills: recapOutBills,
 };
 
